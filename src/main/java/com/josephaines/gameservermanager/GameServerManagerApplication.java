80f15a9973c6fad74e5561783dae7d57dfe89e76
bushycargo <josephaines03@gmail.com>
2022-12-19 19:19:19 +0000

    Inital Commit

---
+++ src/main/java/com/josephaines/gameservermanager/GameServerManagerApplication.java
@@ -0,0 +1,13 @@
+package com.josephaines.gameservermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameServerManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameServerManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/josephaines/gameservermanager/GameServerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josephaines.gameservermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameServerManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
